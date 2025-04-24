f3a9febc77b5d0a0acda9d8640343a8df5f3e445
Per Lindgren <per.lindgren@ltu.se>
2025-04-24 23:18:33 +0200

    working

--- LeanVips/Basic.lean
+++ LeanVips/Basic.lean
@@ -1,19 +1,7 @@
--- BitVec type aliases
+import LeanVips.Reg.Basic
 
-abbrev Reg  : Type := BitVec 5
 abbrev Op   : Type := BitVec 6
 abbrev Funct: Type := BitVec 6
-abbrev Bv32:  Type := BitVec 32
-abbrev Bv16:  Type := BitVec 16
-abbrev Bv26:  Type := BitVec 26
-
-abbrev Regfile : Type := Vector Bv32 32
-
-def clock (rf: Regfile) (a b w: Reg) (d: Bv32) (we: Bool) : Regfile ×  Bv32 × Bv32 :=
-  if we then
-    (rf.set w.toNat d, rf[a.toNat], rf[b.toNat])
-  else
-    (rf, rf[a.toNat], rf[b.toNat])
 
 inductive R where
   | and : R
@@ -28,50 +16,19 @@ inductive I where
   | ori  : I
   | addi : I
   | slti : I
+  | lw   : I
+  | sw   : I
+  | beq  : I
+  | bne  : I
 deriving Repr
 
 inductive Instr where
   | i (instr: I) (rs rt: Reg) (imm: Bv16) : Instr
   | r (instr: R) (rs rt rd: Reg) : Instr
   | j (imm26: Bv26) : Instr
-deriving Repr
+deriving Repr, Inhabited
 
--- register definitions
-@[match_pattern]
-def zero : Reg := 0
-def at'  : Reg := 1
-def v0   : Reg := 2
-def v1   : Reg := 3
-def a0   : Reg := 4
-def a1   : Reg := 5
-def a2   : Reg := 6
-def a3   : Reg := 7
-def t0   : Reg := 8
-def t1   : Reg := 9
-def t2   : Reg := 10
-def t3   : Reg := 11
-def t4   : Reg := 12
-def t5   : Reg := 13
-def t6   : Reg := 14
-def t7   : Reg := 15
-def s0   : Reg := 16
-def s1   : Reg := 17
-def s2   : Reg := 18
-def s3   : Reg := 19
-def s4   : Reg := 20
-def s5   : Reg := 21
-def s6   : Reg := 22
-def s7   : Reg := 23
-def t8   : Reg := 24
-def t9   : Reg := 25
-def k0   : Reg := 26
-def k1   : Reg := 27
-def gp   : Reg := 28
-def sp   : Reg := 29
-def fp   : Reg := 30
-def ra   : Reg := 31
-
--- instruction shorthands
+-- Instruction shorthands
 def and  (rd rs rt: Reg) : Instr := .r .and rd rs rt
 def or   (rd rs rt: Reg) : Instr := .r .or  rd rs rt
 def sub  (rd rs rt: Reg) : Instr := .r .sub rd rs rt
@@ -80,78 +37,71 @@ def andi (rt rs: Reg) (imm16: Bv16): Instr := .i .andi rt rs imm16
 def ori  (rt rs: Reg) (imm16: Bv16): Instr := .i .ori  rt rs imm16
 def addi (rt rs: Reg) (imm16: Bv16): Instr := .i .addi rt rs imm16
 def slti (rt rs: Reg) (imm16: Bv16): Instr := .i .slti rt rs imm16
+def lw   (rt rs: Reg) (imm16: Bv16): Instr := .i .lw   rt rs imm16
+def sw   (rt rs: Reg) (imm16: Bv16): Instr := .i .sw   rt rs imm16
+def beq  (rt rs: Reg) (imm16: Bv16): Instr := .i .beq  rt rs imm16
+def bne' (rt rs: Reg) (imm16: Bv16): Instr := .i .bne  rt rs imm16
 
--- register file
-def rf: Regfile := Vector.mkVector 32 0
-def w (rf: Regfile) (r: Reg) (v: Bv32) : Regfile :=
-  rf.set r.toNat v
-def r (rf: Regfile) (r: Reg) : Bv32 :=
-  match r with
-  | zero => 0 -- register zero always reads zero
-  | _    => rf[r.toNat]
-
--- instruction memory
+-- Instruction memory
 abbrev IMem : Type := Array Instr
 deriving instance Repr for IMem
 
-def instr (im: IMem) (pc: Bv32) : Instr := im[(pc >>> 2).toNat]
+def instr (im: IMem) (pc: Bv32) : Instr := im[(pc >>> 2).toNat]!
 
--- data memory
+-- Data memory
 abbrev DMem : Type := Array Bv32
 deriving instance Repr for DMem
 
-def data (dm: DMem) (addr: Bv32) : Bv32 := dm[(addr >>> 2).toNat]!
+def DMem.w (dm: DMem) (addr: Bv32) (v: Bv32) : DMem :=
+  dm.set! addr.toNat v
+
+def DMem.r (dm: DMem) (addr: Bv32) : Bv32 :=
+  dm[addr.toNat]!
 
 
-def eval (im: IMem) (fuel: Nat) (pc: Bv32) (rf:Regfile) : Regfile :=
+def eval (im: IMem) (fuel: Nat) (pc: Bv32) (rf:Regfile) (dm: DMem): Regfile × DMem × Bv32 :=
   match fuel with
-  | 0 => rf
+  | 0 => (rf, dm, pc)
   | fu + 1 =>
-    let rf := match instr im pc with
-      | .r op rd rs rt =>
-           let a := rf[rs.toNat]
-         let b := rf[rt.toNat]
-         let rd := rd.toNat
-         match op with
-         | .and => rf.set rd (a &&& b)
-         | .or  => rf.set rd (a ||| b)
-         | .add => rf.set rd (a + b)
-         | .sub => rf.set rd (a - b)
-         | .slt => rf.set rd (
-           if a.toInt < b.toInt then
-             1
-           else
-             0
-         )
-      |  .i op rt rs imm16 =>
-        let a := rf[rs.toNat]
-        let rt := rt.toNat
-        match op with
-        | .andi => rf.set rt (a &&& imm16.zeroExtend 32)
-        | .ori  => rf.set rt (a ||| imm16.zeroExtend 32)
-        | .addi => rf.set rt (a + imm16.signExtend 32)
-        | .slti => rf.set rt (
-          if a.toInt < (imm16.signExtend 32).toInt then
-            1
-          else
-            0
-         )
-      | .j imm26 => rf
-     -- | _ => rf
-    eval im fu (pc + 4) rf
+    let (rf, dm, pc) := match instr im pc with
+      | .r op rd rs rt => -- R type instructions
+         let a := rf.r rs
+         let b := rf.r rt
+         (match op with
+         | .and => rf.w rd (a &&& b)
+         | .or  => rf.w rd (a ||| b)
+         | .add => rf.w rd (a + b)
+         | .sub => rf.w rd (a - b)
+         | .slt => rf.w rd (if a < b then 1 else 0), dm, pc)
+      | .i .sw  rt rs imm16 => (rf, dm.w (rf.r rs + imm16.signExtend _) (rf.r rt), pc)
+      | .i .beq rt rs imm16 => (rf, dm, if rf.r rs == rf.r rt then pc + (imm16.signExtend _ <<< 2) else pc)
+      | .i .bne rt rs imm16 => (rf, dm, if rf.r rs != rf.r rt then pc + (imm16.signExtend _ <<< 2) else pc)
+      | .i op rt rs imm16 => -- I type instructions
+        let a := rf.r rs
+        (match op with
+        | .andi => rf.w rt (a &&& imm16.zeroExtend _)
+        | .ori  => rf.w rt (a ||| imm16.zeroExtend _)
+        | .addi => rf.w rt (a + imm16.signExtend _)
+        | .slti => rf.w rt (if a < imm16.signExtend _ then 1 else 0)
+        | .lw   => rf.w rt (dm.r (a + imm16.signExtend _))
+        | _     => unreachable!
+        , dm, pc)
+      | .j _imm26 => panic!("not implemented")
+    eval im fu (pc + 4) rf dm
 
 def pc : Bv32 := 4
+def rf: Regfile := Vector.mkVector 32 0
 def imem : IMem := #[
+  bne'  t2 t2 1,
   addi t0 t0 0x20,
   addi t1 t0 (-1),
   slt  t2 t0 t1,
   slt  t3 t1 t0
 ]
+def dm : DMem := #[]
 
-#eval! ((eval imem 4 0 rf)[t0.toNat])
-#eval! ((eval imem 4 0 rf)[t1.toNat])
-#eval! ((eval imem 4 0 rf)[t2.toNat])
-#eval! ((eval imem 4 0 rf)[t3.toNat])
-
-#eval clock rf 0 1 0 0xdead true
-#check Vector.mkVector
+#eval (eval imem 4 0 rf dm)
+-- #eval ((eval imem 4 0 rf dm)[t1.toNat])
+-- #eval ((eval imem 4 0 rf dm)[t2.toNat])
+-- #eval ((eval imem 4 0 rf dm)[t3.toNat])
+-- #eval ((eval imem 5 0 rf dm)[t3.toNat])
---
+++ LeanVips/Reg/Basic.lean
@@ -0,0 +1,52 @@
+-- BitVectors
+abbrev Bv32:  Type := BitVec 32
+abbrev Bv16:  Type := BitVec 16
+abbrev Bv26:  Type := BitVec 26
+
+-- Registers
+abbrev Reg  : Type := BitVec 5
+
+@[match_pattern]
+def zero : Reg := 0
+def at'  : Reg := 1
+def v0   : Reg := 2
+def v1   : Reg := 3
+def a0   : Reg := 4
+def a1   : Reg := 5
+def a2   : Reg := 6
+def a3   : Reg := 7
+def t0   : Reg := 8
+def t1   : Reg := 9
+def t2   : Reg := 10
+def t3   : Reg := 11
+def t4   : Reg := 12
+def t5   : Reg := 13
+def t6   : Reg := 14
+def t7   : Reg := 15
+def s0   : Reg := 16
+def s1   : Reg := 17
+def s2   : Reg := 18
+def s3   : Reg := 19
+def s4   : Reg := 20
+def s5   : Reg := 21
+def s6   : Reg := 22
+def s7   : Reg := 23
+def t8   : Reg := 24
+def t9   : Reg := 25
+def k0   : Reg := 26
+def k1   : Reg := 27
+def gp   : Reg := 28
+def sp   : Reg := 29
+def fp   : Reg := 30
+def ra   : Reg := 31
+
+-- Register file
+
+abbrev Regfile : Type := Vector Bv32 32
+
+def Regfile.w (rf: Regfile) (r: Reg) (v: Bv32) : Regfile :=
+  rf.set r.toNat v
+def Regfile.r (rf: Regfile) (r: Reg) : Bv32 :=
+  match r with
+  | zero => 0 -- register zero always reads zero
+  | _    => rf[r.toNat]
